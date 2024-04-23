5618397da52cee56a3890cbee9977fd492d204f2
Hussein Haydar <82262862+hussseinhaiadr6@users.noreply.github.com>
2024-04-23 16:07:57 +0200

    Update variables.py

--- Task1/variables.py
+++ Task1/variables.py
@@ -1,3 +1,9 @@
-Path="C:/Users/HHR6/PycharmProjects/"
+Path="C:/Users/HHR6/Desktop/trialcode/Document_Intelligence/"
 def get_path():
     return Path
+
+
+poppler_path = "C:/Users/HHR6/PycharmProjects/Task1/poppler/poppler-24.02.0/Library/bin"
+
+def get_poppler():
+    return poppler_path
